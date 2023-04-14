7bfd26e5adfe977ba75f3d63323b420008347131
tpercival01 <thomaspercival@live.co.uk>
2023-04-14 20:51:56 +0100

    created link validation

--- src/App.js
+++ src/App.js
@@ -7,18 +7,29 @@ import { useState } from "react";
 const App = () => {
   const [links, setLinks] = useState([]);
 
-  const handleShorten = async () => {
+  const handleShorten = async (e) => {
     let link = document.getElementById("link-input").value;
 
-    await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const linkObj = {
-          shorten: data.result["full_short_link"],
-          original: link,
-        };
-        setLinks([...links, linkObj]);
-      });
+    if (link) {
+      await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const linkObj = {
+            shorten: data.result["full_short_link"],
+            original: link,
+          };
+          setLinks([...links, linkObj]);
+        });
+    } else {
+      document.getElementById("warning").style.display = "block";
+      document.getElementById("link-input").style.border = "1px solid red";
+      document.getElementById("link-input").style.color = "red";
+      setTimeout(() => {
+        document.getElementById("warning").style.display = "none";
+        document.getElementById("link-input").style.border = "none";
+        document.getElementById("link-input").style.color = "";
+      }, 1000);
+    }
   };
 
   return (
--- src/components/link-shorten.js
+++ src/components/link-shorten.js
@@ -2,8 +2,9 @@ import BG from "../images/bg-shorten-desktop.svg";
 
 const LinkShorten = (props) => {
   return (
-    <div id="link-shorten-container" style={{ backgroundImage: `url(${BG})` }}>
-      <input id="link-input" type="text" placeholder="Shorten a link here..." />
+    <div id='link-shorten-container' style={{ backgroundImage: `url(${BG})` }}>
+      <div id='warning'>Please add a link</div>
+      <input id='link-input' type='text' placeholder='Shorten a link here...' />
       <span onClick={props.click}>Shorten It!</span>
     </div>
   );
